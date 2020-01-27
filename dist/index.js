437eae137b3c2832d4576251315da5d2268b91a0
jaggs07 <jagaran@fusemachines.com>
2020-01-27 19:03:26 +0545

    minor changes

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Card", {
+  enumerable: true,
+  get: function get() {
+    return _Card.default;
+  }
+});
+Object.defineProperty(exports, "Input", {
+  enumerable: true,
+  get: function get() {
+    return _Input.default;
+  }
+});
+
+var _Card = _interopRequireDefault(require("./components/Card"));
+
+var _Input = _interopRequireDefault(require("./components/Input"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
