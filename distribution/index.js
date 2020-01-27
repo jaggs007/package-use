5b46595c498e0bdb1d42635810f75a4e2ab09ba6
jaggs07 <jagaran@fusemachines.com>
2020-01-27 15:27:00 +0545

    Add files to package.json

--- distribution/index.js
+++ distribution/index.js
@@ -9,15 +9,5 @@ var _Card = _interopRequireDefault(require("./components/Card"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-// ReactDOM.render(<App />, document.getElementById('root'));
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
 var _default = _Card.default;
 exports.default = _default;
