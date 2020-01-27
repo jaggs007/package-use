6feffd22250c9fdc28f655fd9e5697077ce6bb7a
jaggs07 <jagaran@fusemachines.com>
2020-01-27 17:36:48 +0545

    Add css for card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import './index.css';
+import React from 'react'
+import './index.css'
 
 const Card = (props) => {
+  const { title, description, footer } = props
   return (
-    <div className="card">
-      <div>This is the custom card</div>
-      {props.children}
+    <div className="Card">
+      <h1 className='Card-title'>{title}</h1>
+      <div className='Card-description'>{description}</div>
+      <div className='Card-footer'>{footer}</div>
     </div>
-  );
-};
+  )
+}
 
 export default Card;
